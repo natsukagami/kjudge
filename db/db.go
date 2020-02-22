345a61747c24175f62119dc13ce732331b61ea37
Natsu Kagami <natsukagami@gmail.com>
2020-02-22 18:31:16 -0500

    Use a generic Context to support passing Tx and DB

--- db/db.go
+++ db/db.go
@@ -1,12 +1,23 @@
 package db
 
 import (
+	"database/sql"
 	"fmt"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/pkg/errors"
 )
+
+// DBContext defines a slim common interface between Db and Tx.
+type DBContext interface {
+	// Get binds a single row into a struct.
+	Get(result interface{}, query string, args ...interface{}) error
+	// Select binds rows into an array of structs.
+	Select(results interface{}, query string, args ...interface{}) error
+	// Exec executes a query.
+	Exec(query string, args ...interface{}) (sql.Result, error)
+}
 
 // DB is an implementation of the underlying DB.
 type DB struct {
