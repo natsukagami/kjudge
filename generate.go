bb184641ca96a21217b37ff19e1037b5c7e207e4
Natsu Kagami <natsukagami@gmail.com>
2020-02-25 18:24:02 -0500

    Initialize templates folder

--- generate.go
+++ generate.go
@@ -1,4 +1,5 @@
-//go:generate fileb0x fileb0x.yaml
 //go:generate go run models/generate/main.go
+//go:generate sh -c "cd templates && yarn && yarn build"
+//go:generate fileb0x fileb0x.yaml
 
 package kjudge
