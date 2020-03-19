261e648cebadcf432d8ca2c9c5b33747fac6712f
Natsu Kagami <natsukagami@gmail.com>
2020-03-18 21:36:56 -0400

    Implement Submission page

--- frontend/purgecss.config.js
+++ frontend/purgecss.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     content: ["html/**/*.html", "html/*.html"],
-    whitelist: [],
+    whitelist: [/^hljs.*/],
     defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
 };
