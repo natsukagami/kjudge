7d77613b52e3edc7ec3e0434b9c4bbc2eb1b0448
Natsu Kagami <natsukagami@gmail.com>
2020-04-04 01:51:03 +0000

    We are 0.3.0 now (gitea-81)

--- doc.go
+++ doc.go
@@ -5,6 +5,6 @@ package kjudge
 // The version and codename of the system.
 // We typically pick an anime JK name as the codename for every minor version.
 const (
-	Version  = "0.2.0"
-	Codename = "Misaka Mikoto"
+	Version  = "0.3.0"
+	Codename = "Kuroko Shirai"
 )
