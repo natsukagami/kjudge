794417511f1eb92a7e7dae4354c4cb193649e978
Natsu Kagami <natsukagami@gmail.com>
2020-04-17 13:28:22 -0400

    We're 0.5.0 now (#7)

--- doc.go
+++ doc.go
@@ -5,6 +5,6 @@ package kjudge
 // The version and codename of the system.
 // We typically pick an anime JK name as the codename for every minor version.
 const (
-	Version  = "0.4.1"
-	Codename = "Misaki Shokuhou"
+	Version  = "0.5.0"
+	Codename = "Wannai Kinuho"
 )
