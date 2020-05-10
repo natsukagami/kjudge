55c96f3dae94bb5ae4b698ad2c180c87e3f70331
Natsu Kagami <natsukagami@gmail.com>
2020-05-10 17:08:00 -0400

    Run formatter

--- frontend/ts/problem.ts
+++ frontend/ts/problem.ts
@@ -53,28 +53,42 @@ import hd from "humanize-duration";
 
 // Submit form seconds-between-submissions
 (() => {
-    const submitForm = document.getElementById("submit-form") as HTMLFormElement;
-    const dataDiv = submitForm.getElementsByClassName("data")[0] as HTMLDivElement | undefined;
+    const submitForm = document.getElementById(
+        "submit-form",
+    ) as HTMLFormElement;
+    const dataDiv = submitForm.getElementsByClassName("data")[0] as
+        | HTMLDivElement
+        | undefined;
     // Only continue if we have a data-div
     if (!dataDiv) return;
 
-    const lastSubmissionTime = new Date(dataDiv.getAttribute("data-last-submission-time") ?? "");
-    const secondsBetweenSubmissions = Number(dataDiv.getAttribute("data-seconds-between-submissions"));
-    const nextSubmitTime = new Date(lastSubmissionTime.getTime() + secondsBetweenSubmissions * 1000);
+    const lastSubmissionTime = new Date(
+        dataDiv.getAttribute("data-last-submission-time") ?? "",
+    );
+    const secondsBetweenSubmissions = Number(
+        dataDiv.getAttribute("data-seconds-between-submissions"),
+    );
+    const nextSubmitTime = new Date(
+        lastSubmissionTime.getTime() + secondsBetweenSubmissions * 1000,
+    );
 
     // Set a submit handler
-    submitForm.addEventListener("submit", e => {
+    submitForm.addEventListener("submit", (e) => {
         const now = new Date();
-        if (now.getTime() < nextSubmitTime.getTime())
-            e.preventDefault();
-    })
+        if (now.getTime() < nextSubmitTime.getTime()) e.preventDefault();
+    });
 
     // Set a "disable submit handler"
-    const submit = submitForm.getElementsByClassName("submit")[0] as HTMLInputElement;
+    const submit = submitForm.getElementsByClassName(
+        "submit",
+    )[0] as HTMLInputElement;
     const checkSubmit = () => {
         const now = new Date();
         if (now.getTime() < nextSubmitTime.getTime()) {
-            submit.value = `Please wait ${hd(nextSubmitTime.getTime() - now.getTime(), { largest: 1, units: ['m', 's'], round: true })}`;
+            submit.value = `Please wait ${hd(
+                nextSubmitTime.getTime() - now.getTime(),
+                { largest: 1, units: ["m", "s"], round: true },
+            )}`;
             submit.classList.add("cursor-not-allowed");
             submit.disabled = true;
             submit.classList.remove("hover:bg-green-300");
@@ -84,7 +98,7 @@ import hd from "humanize-duration";
             submit.disabled = false;
             submit.classList.add("hover:bg-green-300");
         }
-    }
+    };
     setInterval(checkSubmit, 1000);
     checkSubmit();
 })();
