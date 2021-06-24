8aa91f8e6b82e6fa000c8d67151e9de7f82e59a8
Natsu Kagami <nki@nkagami.me>
2021-06-24 14:48:10 +0900

    Bump minor version

--- doc.go
+++ doc.go
@@ -5,6 +5,6 @@ package kjudge
 // The version and codename of the system.
 // We typically pick an anime JK name as the codename for every minor version.
 const (
-	Version  = "0.7.1"
+	Version  = "0.7.2"
 	Codename = "Maaya Awatsuki"
 )
