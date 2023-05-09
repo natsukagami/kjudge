d0654ae2fba390b97e2640c00ca9ad8c211182ba
Natsu Kagami <nki@nkagami.me>
2023-05-09 13:28:26 +0200

    Fix all scripts with shellcheck

--- scripts/setup_db.sh
+++ scripts/setup_db.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 set -e
 
 FILE=${FILE:-kjudge.db}
 
-if test -f "$FILE"; then
+if test -f "${FILE}"; then
     echo "Setting up the database will override all your unwritten database data."
-    echo "Backing up as $FILE.bak"
-    mv $FILE $FILE.bak
+    echo "Backing up as ${FILE}.bak"
+    mv "${FILE}" "${FILE}".bak
 fi
 
-go run cmd/migrate/main.go -file $FILE < "test/data.sql"
+go run cmd/migrate/main.go -file "${FILE}" < "test/data.sql"
